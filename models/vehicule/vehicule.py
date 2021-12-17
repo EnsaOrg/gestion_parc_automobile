d4c947b37ee7c02ab007bcbbca17968dad3fae59
ambou-97 <ambroise.tine@gmail.com>
2021-12-17 20:38:21 +0100

    char --> Selection

--- models/vehicule/vehicule.py
+++ models/vehicule/vehicule.py
@@ -9,7 +9,7 @@ class ParcAutomobileVehicule(models.Model):
      date_circulation = fields.Date('Date de circulation')
      date_reforme = fields.Date('Date enregistrement')
      kilometrage = fields.Integer()
-     couleur = fields.Char('Couleur')
+     couleur = fields.Selection([('blanc','Blanc'),('noir','Noir'),('argent','Argent'),('gris','Gris'),('rouge','Rouge'),('rouge','Rouge')])
 
      marque_id = fields.Many2one(comodel_name='parc_automobile.marque', delegate=True, required=True)
      parc_id = fields.Many2one(comodel_name='parc_automobile.parc_automobile', delegate=True, required=True)

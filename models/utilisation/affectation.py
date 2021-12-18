9150faf221cba5031ed0d11b4264db761368c7db
ambou-97 <ambroise.tine@gmail.com>
2021-12-18 14:20:21 +0100

    barre d'état dans Odoo

--- models/utilisation/affectation.py
+++ models/utilisation/affectation.py
@@ -10,6 +10,8 @@ class ParcAutomobileAffectation(models.Model):
      secteur = fields.Char('Secteur')
      direction = fields.Char('Direction')
 
+     state = fields.Selection([('l1','Programmé'),('l2','En cours'),('l3','Terminé')])
+
      vehicule_id = fields.Many2one(comodel_name='parc_automobile.vehicule', delegate=True, required=True)
 
      conducteur_ids = fields.Many2many(comodel_name='parc_automobile.conducteur',
@@ -30,3 +32,13 @@ class ParcAutomobileAffectation(models.Model):
      @api.one
      def comp_conducteur(self):
          self.nbr_conducteur = len(self.conducteur_ids)
+
+     @api.multi
+     def next_level(self):
+         if self.state == 'l1':
+             return self.write({'state': 'l2'})
+         elif self.state == 'l2':
+             return self.write({'state': 'l3'})
+         elif self.state == 'l3':
+             return {'warning': {'title': 'Terminé',
+                                 'message': 'This student has already finished his courses!'}}
